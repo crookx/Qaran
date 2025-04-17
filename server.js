6674049f76d5d02db9fede05adb09e1b60cd7e2e
crookx <crookx089@gmail.com>
2025-04-17 10:50:58 -0400

    fix: Update CORS configuration and remove duplicate API paths

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.use(cors({
   origin: 'https://qaranbaby.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
+  credentials: false,
   optionsSuccessStatus: 204
 }));
 
