ff2ebb64f8264f0050bd09282ffdc0f79c4d8910
crookx <crookx089@gmail.com>
2025-04-21 10:57:56 -0400

    Update frontend configuration and CORS settings

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 const app = express();
+
+const ALLOWED_ORIGINS = [
+  'https://baby-shop-mcqv.vercel.app',
+  'https://baby-shop-mcqv-git-master-crookxs-projects.vercel.app',
+  'http://localhost:3000'
+];
+
+// Security middleware
+app.use(helmet());
+
+// CORS configuration
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || ALLOWED_ORIGINS.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'Accept',
+    'Origin',
+    'X-Requested-With'
+  ],
+  credentials: true,
+  maxAge: 86400
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Export the app instance to be configured in server.js
 module.exports = app;
